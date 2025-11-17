72aa6f54cb4a278eebff1a81ba83ed4986071e85
oktamovshokhjahon <oktamovshokhjahon@gmail.com>
2025-11-17 12:28:40 +0500

    urlencoded

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 // Serve static files (for uploaded images)
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
