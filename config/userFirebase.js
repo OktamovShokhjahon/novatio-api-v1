3418e340b4cfe6fd5bae6bba7aed91f4c23ee358
oktamovshokhjahon <oktamovshokhjahon@gmail.com>
2025-11-18 20:11:33 +0500

    firebaes

--- config/userFirebase.js
+++ config/userFirebase.js
@@ -1,10 +1,10 @@
 const admin = require("firebase-admin");
 const serviceAccount = require("./firebase-service-account-user.json");
 
-// if (!admin.apps.length) {
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-// }
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
 module.exports = admin;
