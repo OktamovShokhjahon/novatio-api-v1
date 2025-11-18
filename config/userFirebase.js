33011fd65919e91d8acdb32c384314cf510db888
oktamovshokhjahon <oktamovshokhjahon@gmail.com>
2025-11-18 20:21:03 +0500

    maraz

--- config/userFirebase.js
+++ config/userFirebase.js
@@ -1,7 +1,7 @@
 const admin = require("firebase-admin");
 const serviceAccount = require("./firebase-service-account-user.json");
 
-console.log(admin.apps.length);
+console.log(admin.apps);
 
 // if (!admin.apps.length) {
 admin.initializeApp(
