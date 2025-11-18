db1a383473c67b2b9450b0bb61447673e5b1314f
oktamovshokhjahon <oktamovshokhjahon@gmail.com>
2025-11-18 20:23:54 +0500

    init

--- config/userFirebase.js
+++ config/userFirebase.js
@@ -1,17 +1,17 @@
 const admin = require("firebase-admin");
 const serviceAccount = require("./firebase-service-account-user.json");
 
-console.log(admin.apps.length);
-
 const appName = "messaging";
 
-if (!admin.apps.find((app) => app.name === appName)) {
-  admin.initializeApp(
-    {
-      credential: admin.credential.cert(serviceAccount),
-    },
-    appName
-  );
-}
+// if (!admin.apps.find((app) => app.name === appName)) {
+admin.initializeApp(
+  {
+    credential: admin.credential.cert(serviceAccount),
+  },
+  appName
+);
+// }
+
+console.log(admin.apps.length);
 
 module.exports = admin;
